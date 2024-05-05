86d84c13437220f82579722559cef94c2d4c4ae4
panditprogrammer <panditprogrammer@gmail.com>
2024-05-05 14:48:46 +0530

    WorkF Initial commit

---
+++ android/app/src/main/kotlin/com/example/workf/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.workf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+  const Home({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+
+   return Scaffold(
+    appBar: AppBar(title: Text("WorkF")),
+    body: Container(child: Text("This is WorkF Home"),
+    ),
+   );
+  }
+}
+
+
+
